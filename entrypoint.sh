17c75b08b5d8c6e580560f56c7dadd3310b518ca
Lucas Costi <lucascosti@github.com>
2020-03-24 16:29:09 +1000

    Try Packages push with no username

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "This is the awesome wisdom of the Octocat:"
+echo "This is the really awesome wisdom of the Octocat:"
 curl --request GET --url https://api.github.com/octocat --silent
