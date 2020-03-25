e0206f9d2f8e2a87233c25d1b1d9e0c6b9dcaf2b
Lucas Costi <lucascosti@github.com>
2020-03-25 15:18:30 +1000

    Demo for both Docker Hub and GitHub Packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "THis was published only to GitHub Packages."
+echo "This was published to Docker Hub and GitHub Packages."
 echo "This is the really awesome wisdom of the Octocat:"
 curl --request GET --url https://api.github.com/octocat --silent
